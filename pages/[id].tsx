07562be01a1d3f4e8fbea780e39bc0f77b919b28
whoyoux <48690153+whoyoux@users.noreply.github.com>
2022-06-25 21:04:50 +0200

    More fixes

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -13,20 +13,20 @@ const Asset: NextPage<AssetType> = ({ fileURL }) => {
     return (
         <>
             <Head>
-                <title>Image Uploader</title>
+                <title>Image Uploader </title>
 
-                <meta property="og:url" content="https://www.whx.world/" />
+                <meta property="og:url" content={document.URL} />
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="Image Uploader whx.world" />
                 <meta property="og:description" content="" />
-                <meta property="og:image" content={`${fileURL}`} />
+                <meta property="og:image" content={fileURL} />
 
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta property="twitter:domain" content="whx.world" />
-                <meta property="twitter:url" content="https://www.whx.world/" />
+                <meta property="twitter:url" content={document.URL} />
                 <meta name="twitter:title" content="Image Uploader whx.world" />
                 <meta name="twitter:description" content="" />
-                <meta name="twitter:image" content={`${fileURL}`} />
+                <meta name="twitter:image" content={fileURL} />
             </Head>
             {fileURL ? <img src={fileURL} alt="" /> : <h1>Not found!</h1>}
         </>
